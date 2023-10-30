7bb047250ee8345eda90cc3b0109da28259a32f2
amilcar <alexandercolop7@gmail.com>
2023-10-29 21:07:36 -0600

    prueba

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de Inscripción</title>
+    <script>
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fecha
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numero
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigo
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigoSeguridad").value;
+
+            if (numeroTarjeta === "" || fechaExpiracion === "" || codigoSeguridad === "") {
+                alert("Por favor, complete todos los campos de la tar
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigoSeguridad").value;
+
+            if (numeroTarjeta === "" || fechaExpiracion === "" || codigoSeguridad === "") {
+                alert("Por favor, complete todos los campos de
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigoSeguridad").value;
+
+            if (numeroTarjeta === "" || fechaExpiracion === "" || codigoSeguridad === "") {
+                alert("Por favor, complete todos los campos
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigoSeguridad").value;
+
+            if (numeroTarjeta === "" || fechaExpiracion === "" || codigoSeguridad === "") {
+                alert("Por favor
+
+        function validarFormulario() {
+            // Validación de campos generales
+            var nombre = document.getElementById("nombre").value;
+            var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+            var direccion = document.getElementById("direccion").value;
+            var telefono = document.getElementById("telefono").value;
+            var sexo = document.querySelector('input[name="sexo"]:checked');
+
+            if (nombre === "" || fechaNacimiento === "" || direccion === "" || telefono === "" || !sexo) {
+                alert("Por favor, complete todos los campos generales.");
+                return false;
+            }
+
+            // Validación de campos de tarjeta
+            var numeroTarjeta = document.getElementById("numeroTarjeta").value;
+            var fechaExpiracion = document.getElementById("fechaExpiracion").value;
+            var codigoSeguridad = document.getElementById("codigoSeguridad").value;
+
+            if (numeroTarjeta === "" || fechaExpiracion === "" || codigoSeguridad === "") {
+                alert("Por favor, complete todos los campos de la tarjeta.");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
+
+</head
+</head>
+<body>
+    <h1>Formulario de Inscripción</h1>
+    <form onsubmit="return validarFormulario();">
+        <h2>Datos Generales</h2>
+        
+        <
+
+       
+<label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required><br>
+
+        <label for="fechaNacimiento">Fecha de Nacimiento:</label>
+        <input type="date" id="fechaNacimiento" name="fechaNacimiento" required><br>
+
+        <label for="direccion">Dirección:</label>
+        <input type="text" id="direccion" name="direccion" required><br>
+
+        
+
+        <
+
+
+       
+<label for="telefono">Teléfono:</label>
+        <input type="tel" id="telefono" name="telefono" required><br>
+
+        <label>Sexo:</label>
+        
+        <
+<input type="radio" id="sexoM" name="sexo" value="Masculino" required>
+        <label for="sexoM">Masculino</label>
+        <input type="radio" id="sexoF" name="sexo" value="Femenino" required>
+        
+        <
+<label for="sexoF">Femenino</label><br>
+
+        <h2>Datos de Tarjeta</h2>
+        <label for="numeroTarjeta">Número de Tarjeta:</label>
+        <input type="text" id="numeroTarjeta" name="numeroTarjeta" required><br>
+
+        <label for="fechaExpiracion">Fecha de Expiración:</label>
+        <input type="text" id="fechaExpiracion" name="fechaExpiracion" required><br>
+
+        <label for="codigoSeguridad">Código de Seguridad:</label>
+        <input type="text" id="codigoSeguridad" name="codigoSeguridad" required><br>
+
+        <input type="submit" value="Inscribirse">
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+
+Regenerado
